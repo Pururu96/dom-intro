4152a7c17b899e151e9fcd3efba55e0aad6b08be
Pururu <pururumphahlele@gmail.com>
2021-08-11 15:02:58 +0200

    Addition of behaviour to calculate button.

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,10 +1,28 @@
 //get a reference to the calculate button
-
+const calculateBtnElement = document.querySelector(".calculateBtn")
 //get a reference to the billTotal element
-
+const billTotalElement = document.querySelector(".billTotal")
 //get a reference to the billString
-
+const billStringElement = document.querySelector(".billString");
 //create the function that will be called when the calculate button is pressed
+function calculateBtnClicked() {
+    // logic goes here
+    var callsAndSmsString = billStringElement.value;
+    var callsAndSmss = callsAndSmsString.split(',');
+    //a variable for the total phonebill
+    let billTotal = 0;
+    //Loop over each item
+    for (var i = 0; i < callsAndSmss.length; i++) {
+        const currentCallOrSms = callsAndSmss[i].trim();
+        if (currentCallOrSms.includes('call')) {
+            billTotal += 2.75;
+        }
+        else if (currentCallOrSms.includes('sms')) {
+            billTotal += 0.65;
+        }
+        return 'R' + total.toFixed(2); 
+    }
+}
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
