805a74df7d36b5856b69230c69c4849b9f9f5ff4
Pururu <pururumphahlele@gmail.com>
2021-08-13 13:30:07 +0200

    Add text-bill JS file.

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,37 @@
 // get a reference to the textbox where the bill type is to be entered
-
+var billTypeTextElem = document.querySelector(".billTypeText");
 //get a reference to the add button
-
+var addToBillBtnElem = document.querySelector(".addToBillBtn");
 //create a variable that will keep track of the total bill
-
-//add an event listener for when the add button is pressed
-
+var callTotalOneElem = document.querySelector(".callTotalOne");
+var smsTotalOneElem = document.querySelector(".smsTotalOne");
+var totalOneElem = document.querySelector(".totalOne")
+var callsTotal = 0;
+var smsTotal = 0;
+function textBillTotal(){
+    var billTypeEntered = billTypeTextElem.value.trim();
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+if (billTypeEntered === "call"){
+    callsTotal += 2.75;
+}
+else if (billTypeEntered === "sms"){
+    smsTotal += 0.75;
+}
 // * add the appropriate value to the running total
+callTotalOneElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalOneElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalOneElem.innerHTML = totalCost.toFixed(2);
 // * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+if (totalCost > 50){
+    // red
+    totalOneElem.classList.add("danger");
+}
+//orange
+else if (totalCost >= 30 && totalCost < 50){
+    totalOneElem.classList.add("warning");
+}
+}
+//add an event listener for when the add button is pressed
+addToBillBtnElem.addEventListener('click', textBillTotal);
+
